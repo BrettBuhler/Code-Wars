b4b5d4e0d13944ab92e9aff2ee2d6173533c7c31
BrettBuhler <107363334+BrettBuhler@users.noreply.github.com>
2022-09-26 21:23:34 -0600

    Create decodeTheMorseCode.js

---
+++ JavaScript/6 Kyu/decodeTheMorseCode.js
@@ -0,0 +1,8 @@
+/*
+In this kata you have to write a simple Morse code decoder. While the Morse code is now mostly superseded by voice and digital data communication channels, it still has its use in some applications around the world.
+The Morse code encodes every character as a sequence of "dots" and "dashes". For example, the letter A is coded as ·−, letter Q is coded as −−·−, and digit 1 is coded as ·−−−−. The Morse code is case-insensitive, traditionally capital letters are used. When the message is written in Morse code, a single space is used to separate the character codes and 3 spaces are used to separate words. For example, the message HEY JUDE in Morse code is ···· · −·−−   ·−−− ··− −·· ·.
+*/
+// MY SOLUTION
+const decodeMorse = (code) => {
+    return code.split('   ').map(x=>x.split(' ').map(x=>MORSE_CODE[x]).join('')).join(' ').trim()
+  }
