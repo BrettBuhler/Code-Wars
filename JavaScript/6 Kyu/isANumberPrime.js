10560da49a58fa47748510e2192b0d9c6dac654c
BrettBuhler <107363334+BrettBuhler@users.noreply.github.com>
2023-02-27 17:45:51 -0700

    Create isANumberPrime.js

---
+++ JavaScript/6 Kyu/isANumberPrime.js
@@ -0,0 +1,10 @@
+const isPrime = (n) => {
+    if (n === 2 || n === 3) return true
+    if (n % 2 === 0 || n % 3 === 0) return false
+    for (let i = 5; i * i <= n; i += 6){
+        if(n % i === 0 || n % (i + 2) === 0) return false
+    }
+    return true
+}
+
+console.log(isPrime(7))
