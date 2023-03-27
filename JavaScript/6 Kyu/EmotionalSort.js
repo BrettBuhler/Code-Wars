4c5d371db5e3297816c5506d06daba90511ed9bd
BrettBuhler <107363334+BrettBuhler@users.noreply.github.com>
2023-03-27 09:44:00 -0600

    Create EmotionalSort.js

---
+++ JavaScript/6 Kyu/EmotionalSort.js
@@ -0,0 +1,52 @@
+/*
+Emotional Sort ( ︶︿︶)
+You'll have a function called "sortEmotions" that will return an array of emotions sorted. It has two parameters, the first parameter called "arr" will expect an array of emotions where an emotion will be one of the following:
+
+:D -> Super Happy
+:) -> Happy
+:| -> Normal
+:( -> Sad
+T_T -> Super Sad
+Example of the array:[ 'T_T', ':D', ':|', ':)', ':(' ]
+
+And the second parameter is called "order", if this parameter is true then the order of the emotions will be descending (from Super Happy to Super Sad) if it's false then it will be ascending (from Super Sad to Super Happy)
+
+Example if order is true with the above array: [ ':D', ':)', ':|', ':(', 'T_T' ]
+
+Super Happy -> Happy -> Normal -> Sad -> Super Sad
+If order is false: [ 'T_T', ':(', ':|', ':)', ':D' ]
+
+Super Sad -> Sad -> Normal -> Happy -> Super Happy
+Example:
+
+arr = [':D', ':|', ':)', ':(', ':D']
+sortEmotions(arr, true) // [ ':D', ':D', ':)', ':|', ':(' ]
+sortEmotions(arr, false) // [ ':(', ':|', ':)', ':D', ':D' ]
+More in test cases!
+
+Notes:
+
+The array could be empty, in that case return the same empty array ¯\_( ツ )_/¯
+All emotions will be valid
+*/
+
+const sortEmotions = (arr, order) => {
+    let faces = {
+        1: ':D',
+        2: ':)',
+        3: ':|',
+        4: ':(',
+        5: 'T_T'
+    }
+    let sortedArr = []
+    for (let i = 0; i < 6; i++){
+        for (let j = 0; j < arr.length; j++){
+            if (faces[i] == arr[j]){
+                sortedArr.push(arr[j])
+            }
+        }
+    }
+    return order ? sortedArr : sortedArr.reverse()
+}
+
+console.log(sortEmotions([ ':D', 'T_T', ':D', ':(' ], false))
