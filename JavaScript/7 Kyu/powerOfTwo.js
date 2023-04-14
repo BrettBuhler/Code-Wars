3794dd269a6b8e2b5e48dfa8abd4b1d9348c34fc
BrettBuhler <107363334+BrettBuhler@users.noreply.github.com>
2023-04-13 18:08:51 -0600

    Create powerOfTwo.js

---
+++ JavaScript/7 Kyu/powerOfTwo.js
@@ -0,0 +1,5 @@
+/*
+Complete the function power_of_two/powerOfTwo (or equivalent, depending on your language) that determines if a given non-negative integer is a power of two.
+*/
+
+const isPowerOfTwo = (n) => Math.log2(n)%1===0
