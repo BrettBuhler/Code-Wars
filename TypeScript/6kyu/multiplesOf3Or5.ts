dabda08a2b8da4379409921afec168c9faf8ebc3
BrettBuhler <107363334+BrettBuhler@users.noreply.github.com>
2023-06-02 21:20:21 -0600

    Create multiplesOf3Or5.ts

---
+++ TypeScript/6kyu/multiplesOf3Or5.ts
@@ -0,0 +1,23 @@
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+
+Courtesy of projecteuler.net (Problem 1)
+*/
+
+export class Challenge {
+    static solution(n: number):number {
+        if (n < 0) return 0
+        const numberArray: number[] = []
+        for (let i: number = 3; i < n; i++){
+            if (i % 3 === 0 || i % 5 === 0){
+                numberArray.push(i)
+            }
+        }
+        if (numberArray.length === 0) return 0
+        return numberArray.reduce((a,b)=>a + b)
+    }
+}
