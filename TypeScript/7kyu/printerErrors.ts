8c7ff4fc99de57ad8a9a97d68092b01a848e1c92
BrettBuhler <107363334+BrettBuhler@users.noreply.github.com>
2023-07-07 18:38:51 -0600

    Create printerErrors.ts

---
+++ TypeScript/7kyu/printerErrors.ts
@@ -0,0 +1,28 @@
+/*
+In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+
+The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+
+Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+
+You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+
+The string has a length greater or equal to one and contains only letters from ato z.
+
+Examples:
+s="aaabbbbhaijjjm"
+printer_error(s) => "0/14"
+
+s="aaaxbbbbyyhwawiwjjjwwm"
+printer_error(s) => "8/22" 
+*/
+
+export const printerError = (s: string): string => {
+    return `${s.split('').reduce((a: number, b: string) => {
+      if (b.charCodeAt(0) >= 97 && b.charCodeAt(0) <= 109) {
+        return a
+      } else {
+        return a + 1
+      }
+    }, 0)}/${s.length}`
+  }
