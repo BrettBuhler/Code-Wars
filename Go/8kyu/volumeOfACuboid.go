74c818512789368a221121f0b5abe054a6df46fe
BrettBuhler <107363334+BrettBuhler@users.noreply.github.com>
2023-07-11 16:00:10 -0600

    Create volumeOfACuboid.go

---
+++ Go/8kyu/volumeOfACuboid.go
@@ -0,0 +1,11 @@
+/*
+Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+GEOMETRYFUNDAMENTALSMATHEMATICS
+*/
+
+package kata
+
+func GetVolumeOfCuboid (a, b, c float64) float64 {
+  return a * b * c
+}
