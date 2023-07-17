54aae0ca25696048d4056ea52bcab2b986831634
BrettBuhler <107363334+BrettBuhler@users.noreply.github.com>
2023-07-16 20:01:40 -0600

    Create vampireNumbers.cs

---
+++ C#/7kyu/vampireNumbers.cs
@@ -0,0 +1,43 @@
+/*
+Vampire Numbers
+Our loose definition of Vampire Numbers can be described as follows:
+
+6 * 21 = 126
+# 6 and 21 would be valid 'fangs' for a vampire number as the 
+# digits 6, 1, and 2 are present in both the product and multiplicands
+
+10 * 11 = 110
+# 110 is not a vampire number since there are three 1's in the
+# multiplicands, but only two 1's in the product
+Create a function that can receive two 'fangs' and determine if the product of the two is a valid vampire number.
+*/
+static class Kata {
+    public static bool vampire_test(long x, long y){
+        long z = x * y;
+        string str_x = x.ToString();
+        string str_y = y.ToString();
+        string str_z = z.ToString();
+
+        foreach (char item in str_x) {
+            if (str_z.Contains(item)){
+                int index = str_z.IndexOf(item);
+                str_z = str_z.Remove(index,1);
+            } else {
+                return false;
+            }
+        }
+        foreach (char item in str_y) {
+            if (str_z.Contains(item)){
+                int index = str_z.IndexOf(item);
+                str_z = str_z.Remove(index,1);
+            } else {
+                return false;
+            }
+        }
+        if (str_z.Length == 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
