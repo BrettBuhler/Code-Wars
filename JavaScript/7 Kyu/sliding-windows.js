f6b13673fc00d75deca40ec281bf68c06c8f3e3d
Brett Buhler <107363334+BrettBuhler@users.noreply.github.com>
2025-07-05 13:21:17 -0600

    update window

--- JavaScript/7 Kyu/sliding-windows.js
+++ JavaScript/7 Kyu/sliding-windows.js
@@ -1,16 +1,19 @@
 const window = (len, off, li) => {
     const ret = []
-    for (let i = 0; i < li.length; i+= off) {
-	if (li[len + i] === undefined) {
-	    console.log("undefined")
-	    break
+    for (let i = 0; i <= li.length; i += off){
+	const slice_of_li = li.slice(i, i + len)
+	if (slice_of_li.length == len){
+	    ret.push(slice_of_li)
 	}
-	console.log(li.slice(i, i+len))
-	ret.push(li.slice(i, i+len))
     }
     return ret
 }
 const example_len = 2
 const example_off = 1
 const example_li = [0, 1, 2, 3, 4]
-console.log(`Test: ${window(example_len, example_off, example_li)}`)
+const example_result = window(example_len, example_off, example_li)
+console.log(example_result)
+for (ex in example_result)  {
+    console.log(ex)
+}
+
